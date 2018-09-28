6f55bd9746b46c10bdd9a21784d5d564e8518fbe
Julie K <julieskim160@gmail.com>
2018-09-27 21:16:34 -0700

    showing employee profile, need styling

--- index.js
+++ index.js
@@ -1,77 +1,76 @@
 $(document).ready(function() {
 
-    let haveEmployeeSelected = false;
-    const users=[];
-    const userJson= {
-        "emp1" : {
-            name:"Michael Scott",
-            description:"Manager"
-        },
-        "emp2" : {
-            name:"Jim Halpert",
-            description:"Paper Salesman"
-        },
-        "emp3" : {
-            name:"Dwight Shrute",
-            description:"Assistant to the Manager"
-        },
-        "emp4" : {
-            name:"Pam Beesly",
-            description:"Receptionist"
-        },
-        "emp5" : {
-            name:"Angela Martin",
-            description:"Accountant"
-        },
-        "emp6" : {
-            name:"Kevin Malone",
-            description:"Accountant"
-        },
-        "emp7" : {
-            name:"Kelly Kapoor",
-            description:"Customer Service Rep"
-        },
-        "emp8" : {
-            name:"Ryan Howard",
-            description:"Intern"
-        },
-        "emp9" : {
-            name:"Stanley Hudson",
-            description:"Salesman"
-        },
-        "emp10" : {
-            name:"Andy Bernard",
-            description:"Salesman"
-        },
-        "emp11" : {
-            name:"Meredith Palmer",
-            description:"Supplier Relations"
-        },
-        "emp12" : {
-            name:"Oscar Martinez",
-            description:"Accountant"
-        },
+    //object oriented with logical methods instead of step by step instructions
+    function ProfileItem(employeeJson, id) {
+        this.id = id;
+        this.name = employeeJson[id].name;
+        this.description = employeeJson[id].description;
+        this.url = employeeJson[id].url;
 
-    }
+        this.generateHtml = function() {
+            return `
+                <div>Name: ${this.name}</div>
+                <div>Id: ${this.id}</div>
+                <div>Description: ${this.description} </div>
+                <img src="${this.url}"/>
+            `;
+        }
+    } 
+    //define the engine
+    function ProfileEngine(employeeJson) {
+        this.employeeJson = employeeJson;
+        this.item1; 
+        this.item2;
+        this.item3; 
 
+        this.hasItem = function () {
+            return this.item1 || this.item2 || this.item3;
+        }
 
-    $('.employee-list').on('click', function(e) {
-        console.log(e.target.id)
-        if(!haveEmployeeSelected) {
-            haveEmployeeSelected = true;
-            $('.row').toggleClass('row--show');
+        // adds item
+        this.addItem = function (id) {
+            this.item3 = this.item2; 
+            this.item2 = this.item1; 
+
+            this.item1 = new ProfileItem(this.employeeJson, id); // { id: 'emp1', name: 'julie', description 'sassy af' };
 
         }
-       
-        $('#pos3').html($('#pos2').html());
-        $('#pos2').html($('#pos1').html()); 
-        $('#pos1').html("e.target.name");
 
-    })
+        // renders all items
+        this.render = function() {
+            // <section class="profile-row"></section>
+            // <section class="row-employee" id="pos1">employee1</section>
+            $('#pos1').html(this.item1.generateHtml());
 
-    let data = Object.keys(userJson)
-     //square bracket notation to get data
+            if (this.item2) {
+                $('#pos2').html(this.item2.generateHtml());
+            }
+            if (this.item3) {
+                $('#pos3').html(this.item3.generateHtml());
+            }
+        }
+    };
 
+    const renderAvatars = function(employeeJson) {
+        const avatarHtml = Object.keys(employeeJson).map((id) => { // [ '<li>...</li>', '<li>....]
+            const employee = employeeJson[id];
+            return `<li><img src="${employee.url}" alt="${employee.name}" class="emp-img" id="${id}" /><li>`;
+        }).join('');
+        $('.employee-list').html(avatarHtml);
+    }
+
+    //create engine
+    const profileEngine = new ProfileEngine(EmployeeJson); // { item1: undefined, item2: undefined, item3: undefined } (.hasItem , .addItem, .render methods)
+    //use engine
+    renderAvatars(EmployeeJson);
+
+    $('.employee-list').on('click', function(e) {
+        if(!profileEngine.hasItem()) {
+            $('.profile-row').toggleClass('row--show');
+        }
+        profileEngine.addItem(e.target.id);
+        profileEngine.render();
+    });
 });
 
 
